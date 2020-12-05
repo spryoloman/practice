f6f8eea953a1d67323770352a2302a4b5ba20f7a
Andrew Kulagin <Kulagin>
2020-12-05 23:53:47 +0200

    Firtst Console App

---
+++ console_app_1.js
@@ -0,0 +1,17 @@
+const numberOfFilms = +prompt("How many films have you already watched?", "");
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+const a = prompt("What was your last watched film?", ""),
+      b = prompt("How would you rate it?", ""),
+      c = prompt("What was your last watched film?", ""),
+      d = prompt("How would you rate it?", "");
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
