f28e8e42e0b2f0994006a7f58b654d66029984f0
Andrew Kulagin <Kulagin>
2020-12-06 00:03:11 +0200

    123

---
+++ app1.js
@@ -0,0 +1,21 @@
+'use strict';
+
+const numberOfFilms = +prompt('How many films have you already watched?', '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
+};
+
+const a = prompt('What was your last watched film?', ''),
+      b = prompt('How would you rate it?', ''),
+      c = prompt('What was your last watched film?', ''),
+      d = prompt('How would you rate it?', '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+consolele.log(personalMovieDB);
